bcdea726324529be72401a61e104b5652fd7ce73
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-05-01 19:00:42 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,70 +1,74 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 #include <cassert>
+
+template<typename T>
 class TPQueue {
- private:
-  struct ITEM {
-    T data;
-    ITEM* next;
-    ITEM* prev;
-  };
-  ITEM* head;
-  ITEM* tail;
-  TPQueue::ITEM* create(const T& data, ITEM*prev) {
-    ITEM* item = new ITEM;
-    item->data = data;
-    item->next = nullptr;
-    item->prev = prev;
-    return item;
-  }
+  // Сюда помещается описание структуры "Очередь с приоритетами"
+   private:
+struct ITEM {
+T data;
+ITEM* next;
+ITEM* prev;
+};
+ITEM* head;
+ITEM* tail;
+TPQueue::ITEM* create(const T& data, ITEM*prev) {
+ITEM* item = new ITEM;
+item->data = data;
+item->next = nullptr;
+item->prev = prev;
+return item;
+}
 
  public:
-  TPQueue() :head(nullptr), tail(nullptr) {}
-  ~TPQueue() {
-    while (head)
-      pop();
-  }
-  void push(const T& data) {
-    if (tail && head) {
-      ITEM* current = tail;
-      while (current && data.prior > (current->data).prior) {
-        current = current->prev;
-      }
-      if (current) {
-        ITEM* temp = current->next;
-        current->next = create(data, current);
-        current = current->next;
-        current->next = temp;
-        if (temp)
-          temp->prev = current;
-        else
-          tail = current;
-      } else {
-        current = create(data, nullptr);
-        current->next = head;
-        head->prev = current;
-        head = current;
-      }
-    } else {
-      head = create(data, nullptr);
-      tail = head;
-    }
-  }
-  T pop() {
-    assert(head);
-    ITEM* temp = head->next;
-    T data = head->data;
-    if (temp)
-      temp->prev = nullptr;
-    delete head;
-    head = temp;
-    return data;
-  }
+TPQueue() :head(nullptr), tail(nullptr) {}
+~TPQueue() {
+while (head)
+pop();
+}
+void push(const T& data) {
+if (tail && head) {
+ITEM* current = tail;
+while (current && data.prior > (current->data).prior) {
+current = current->prev;
+}
+if (current) {
+ITEM* temp = current->next;
+current->next = create(data, current);
+current = current->next;
+current->next = temp;
+if (temp)
+temp->prev = current;
+else
+tail = current;
+} else {
+current = create(data, nullptr);
+current->next = head;
+head->prev = current;
+head = current;
+}
+} else {
+head = create(data, nullptr);
+tail = head;
+}
+}
+T pop() {
+assert(head);
+ITEM* temp = head->next;
+T data = head->data;
+if (temp)
+temp->prev = nullptr;
+delete head;
+head = temp;
+return data;
+}
 };
+
 struct SYM {
   char ch;
-  int prior;
+  int  prior;
 };
 
 #endif // INCLUDE_TPQUEUE_H_
